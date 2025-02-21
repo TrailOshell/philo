43cc5cf64860a1f0a2ad42edd97972af55d5d6f5
tsomchan <marvin@42.fr>
2025-02-21 20:58:15 +0700

    edit print_philo_stats() to use red color in timestamp when philosopher is done

--- philo/srcs/debug/debug2.c
+++ philo/srcs/debug/debug2.c
@@ -6,7 +6,7 @@
 /*   By: tsomchan <tsomchan@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 13:22:23 by tsomchan          #+#    #+#             */
-/*   Updated: 2025/02/21 19:11:46 by tsomchan         ###   ########.fr       */
+/*   Updated: 2025/02/21 20:54:32 by tsomchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,18 @@ static void	print_philo_stats(t_data *data, t_philo *philo,
 {
 	const char		*state[7] = {"🤔 ", "🍴 ", "🍝 ", "💤 ", "😃 ", "💀 "};
 	const char		*txt[7] = {WHT, WHT, YLW, BLU, GRN, RED};
+	char			*color;
 	int				i;
-	int				len;
 	int				n_eaten;
 
 	i = philo->id - 1;
 	printf(CYN "p"YLW"%d%s", i + 1, state[get_state(&data->philos[i])]);
-	len = digit_len(get_last_meal_time(philo) / 1000);
 	n_eaten = get_n_eaten(philo);
-	printf(CYN"(");
+	if (get_process(data) == RUNNING)
+		color = CYN;
+	else
+		color = RED;
+	printf("%s(", color);
 	print_empty_digit_len(data->n_ph_eat, n_eaten);
 	if (data->n_ph_eat && n_eaten >= data->n_ph_eat)
 		printf(B_GRN);
@@ -48,7 +51,7 @@ static void	print_philo_stats(t_data *data, t_philo *philo,
 	if (last_meal_time >= die_in_ms + 10)
 		printf(RED);
 	printf("%s", txt[get_state(philo)]);
-	printf("%lu"CYN")", last_meal_time);
+	printf("%lu%s)", last_meal_time, color);
 }
 
 void	db_check_all_ph_ex(void)
